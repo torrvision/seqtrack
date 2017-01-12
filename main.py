00bb988fc932ac1e0163730133eb311e04d9c91b
Namhoon <namhoonlee09@gmail.com>
2017-01-12 15:48:15 +0000

    WIP

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import pdb
 import argparse
 
 from opts           import Opts
-from load_data      import load_data
+#from load_data      import load_data
+from data           import Data_moving_mnist
 from load_model     import load_model
 from train          import train
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='rnn tracking - main script')
-    parser.add_argument('--verbose', help='print arguments', action='store_true')
-    parser.add_argument('--teststr', help='test str argument', type=str, default='')
-    parser.add_argument('--testval', help='test int argument', type=int, default=0)
+    parser.add_argument('--verbose', help='print arguments', 
+            action='store_true')
+    parser.add_argument('--mode', help='experiment mode (train, test)', 
+            type=str, default='train' )
+    parser.add_argument('--dropout_rnn', help='set dropout for rnn', 
+            action='store_true')
     args = parser.parse_args()
 
     # print help and args
@@ -19,18 +23,27 @@ def parse_arguments():
     return parser.parse_args()
 
 if __name__ == "__main__":
+    # parameter settings
     args = parse_arguments()
     o = Opts()
     o.update_by_sysarg(args=args)
 
-    data_tr, data_va, data_te = load_data(o) # if able to load the whole dataset 
+    #TODO: load_data deprecated
+    #data_tr, data_va, data_te = load_data(o) # if able to load the whole dataset 
 
-    model = load_model(o)
+    # Note that depending on the dataset the loading approach can be different
+    if o.dataset == 'moving_mnist': 
+        loader = Data_moving_mnist(o)
+    elif o.dataset =='another_dataset': 
+        raise ValueError('dataset not implemented yet')
+    else: 
+        raise ValueError('wrong dataset')
 
-    '''
-    net = train(model, data_tr, data_va, o)
+    model = load_model(o, loader, is_training=True if o.mode=='train' else False)
 
-    test(model, data_tr, data_va, o)
-    '''
+    train(model, loader, o)
+
+    #TODO: write test model routine; name scope or op scope might be required
+    #test(model, data_tr, data_va, o)
 
     pdb.set_trace()
--- opts.py
+++ opts.py
@@ -1,6 +1,7 @@
 import pdb
 import numpy as np
 import os
+import tensorflow as tf
 
 
 class Opts(object):
@@ -11,35 +12,46 @@ class Opts(object):
         # basic settings 
         self.path_base          = os.path.dirname(__file__)
         self.path_data          = os.path.join(self.path_base, 'data')
-        self.mode               = 'train' # 'train' or 'test'
+        self.mode               = '' # 'train' or 'test'
         self.seed_general       = 9
+        self.dtype              = tf.float32
 
         #----------------------------------------------------------------------
-        # data set
-        self.dataset            = 'moving_mnist' # options: 
+        # data set 
+        self.dataset            = 'moving_mnist' # (moving_mnist, , ,)
+        self.moving_mnist       = {
+                                    'frmsz': 100,
+                                    'featdim': 100*100,
+                                    'outdim': 2 
+                                    }
+
         
         #----------------------------------------------------------------------
-        # model parameters
+        # model parameters - rnn
         self.model              = 'rnn_basic'
         self.cell_type          = 'LSTM' 
-        self.cell_hidden_size   = 300 # or dimension?
+        self.nunits             = 300 # or dimension?
         self.ntimesteps         = 30
         self.nlayers            = 1
-        self.dropout            = False
+        self.dropout_rnn        = False
         self.keep_ratio         = 0.5
+
+        #----------------------------------------------------------------------
+        # model parameters - cnn (or feature extractor)
+        self.model_cnn          = 'vgg' # vgg, resnet, imagenet, etc.
 
         #----------------------------------------------------------------------
         # training policies
         self.nepoch             = 10
-        self.batchsz            = 1
+        self.batchsz            = 16
         self.optimizer          = 'sgd' # sgd, adam, rmsprop
         self.lr                 = 0.001
+        self.lr_update          = False
         self.wd                 = 0.0
         self.grad_clip          = False
         self.max_grad_norm      = 5.0
+        self.regularization     = False # weight regularization!
 
-        # test params
-        self.testval            = 10
     
     def update_by_sysarg(self, args):
         ''' Update options by system arguments
