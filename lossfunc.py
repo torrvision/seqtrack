f15f078dcf6006d824df4805c4719f6328a9ef76
Jack Valmadre <jack.valmadre@gmail.com>
2017-08-09 12:39:25 +0100

    Fix bug in lossfunc
    Bug caused by adding/removing trailing singleton dimension to label map.
    
    
    Former-commit-id: 5dbfd8a97d1473c955b8b80cbcd18e5da0078514

--- lossfunc.py
+++ lossfunc.py
@@ -18,9 +18,11 @@ def score_logistic(mask, score_pred, rect_gt, radius_pos=0.2, radius_neg=0.5,
     with tf.name_scope('score_logistic'):
         score_dim = score_pred.shape.as_list()[-3:-1][::-1]
         label_map = _make_label_map(mask, rect_gt, score_dim, radius_pos, radius_neg)
+        # label_map is [b, t, h, w]
+        # score_pred is [b, t, h, w, 1]
         loss_raw = tf.nn.sigmoid_cross_entropy_with_logits(
             labels=tf.to_float(tf.equal(label_map, 1)),
-            logits=score_pred)
+            logits=tf.squeeze(score_pred, -1))
 
         # Normalize labels per image, then take mean over all images.
         is_pos = tf.equal(label_map, 1)
@@ -36,7 +38,7 @@ def score_logistic(mask, score_pred, rect_gt, radius_pos=0.2, radius_neg=0.5,
         with tf.name_scope('summary'):
             tf.summary.image('label_map',
                 tf.image.convert_image_dtype(
-                    0.5 * (1.0 + tf.to_float(label_map[0])),
+                    tf.expand_dims(0.5 * (1.0 + tf.to_float(label_map[0])), -1),
                     tf.uint8, saturate=True),
                 collections=image_summaries_collections)
             tf.summary.image('score',
@@ -58,35 +60,36 @@ def _make_label_map(mask, rect, dim, radius_pos, radius_neg):
         dim -- Size of score map. Tensor of length 2.
 
     Returns:
-        [b, t, h, w, 1]
+        [b, t, h, w]
 
     It is assumed that the center of the first and last pixel in the score map
     aligns with the first and last pixel in the (cropped) image.
     '''
     eps = 1e-3
-    u, v = tf.meshgrid(tf.to_float(tf.range(dim[0])) / tf.to_float(dim[0] - 1),
-                       tf.to_float(tf.range(dim[1])) / tf.to_float(dim[1] - 1),
-                       indexing='xy')
-    coords = tf.stack([u, v], axis=-1)
+    dim_y, dim_x = dim[0], dim[1]
+    coords_x, coords_y = tf.meshgrid(
+        tf.to_float(tf.range(dim_x)) / tf.to_float(dim_x - 1),
+        tf.to_float(tf.range(dim_y)) / tf.to_float(dim_y - 1))
+    coords = tf.stack([coords_x, coords_y], axis=-1)
     # Use same rectangle for all spatial positions.
     # rect [n, t, 4] -> [n, t, 1, 1, 4]
     rect = tf.expand_dims(tf.expand_dims(rect, 2), 2)
     rect_min, rect_max = geom.rect_min_max(rect)
     rect_center = 0.5 * (rect_min + rect_max)
     rect_size = tf.maximum(rect_max - rect_min, 0) + eps
-    rect_diam = tf.exp(tf.reduce_mean(tf.log(rect_size), axis=-1, keep_dims=True))
-    dist = tf.norm(coords - rect_center, axis=-1, keep_dims=True)
+    rect_diam = tf.exp(tf.reduce_mean(tf.log(rect_size), axis=-1))
+    dist = tf.norm(coords - rect_center, axis=-1)
     rel_dist = dist / rect_diam
     # Ensure that at least the closest element is a positive!
     # (Only if it is not too far in *absolute* distance.)
     min_dist = tf.reduce_min(dist, axis=(-2, -1), keep_dims=True)
+    # pos = rel_dist <= radius_pos
     pos = tf.logical_or(rel_dist <= radius_pos,
                         tf.logical_and(dist <= min_dist, min_dist <= 0.1))
-    neg = tf.logical_and(tf.logical_not(pos),
-                         rel_dist > radius_neg)
+    neg = tf.logical_and(tf.logical_not(pos), rel_dist > radius_neg)
     # Exclude elements that are invalid.
     mask = tf.expand_dims(tf.expand_dims(mask, -1), -1)
     pos = tf.logical_and(pos, mask)
     neg = tf.logical_and(neg, mask)
-    label = tf.cast(pos, tf.int32) - tf.cast(neg, tf.int32)
+    label = tf.to_int32(pos) - tf.to_int32(neg)
     return label
