91fb7f7623d6c94cb6a14d6256e3cfedafe91f61
Namhoon <namhoonlee09@gmail.com>
2017-10-04 11:22:12 +0100

    Fix error in scale-classification ground-truth creation.
    Former-commit-id: 9404faf3a06a882f9a41910c122692ecf49b5fc9

--- train.py
+++ train.py
@@ -682,7 +682,7 @@ def compute_scale_classification_gt(example, outputs):
             sc_gt.append(tf.logical_and(tf.greater_equal(sc_ratio, scales[i-1]), tf.less(sc_ratio, scales[i])))
         elif i == len(scales)/2:
             sc_gt.append(tf.logical_and(tf.greater_equal(sc_ratio, scales[i-1]), tf.less(sc_ratio, scales[i+1])))
-        elif i > len(scales)/2:
+        elif i > len(scales)/2 and not i == len(scales)-1:
             sc_gt.append(tf.logical_and(tf.greater_equal(sc_ratio, scales[i]), tf.less(sc_ratio, scales[i+1])))
         elif i == len(scales)-1:
             sc_gt.append(tf.greater_equal(sc_ratio, scales[i]))
