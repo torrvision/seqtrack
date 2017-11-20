a40990cc38e34b925b48c2b3fb6e30599da229da
Jack Valmadre <jack.valmadre@gmail.com>
2017-11-20 13:15:24 +0000

    Add visualization to track.py
    Former-commit-id: 4eba3521b8c89b7d7b97912b9326648c3e2c89b8

--- track.py
+++ track.py
@@ -24,12 +24,18 @@ def parse_arguments():
         help='e.g. iteration-100000; where iteration-100000.index exists')
     parser.add_argument('out_file', help='e.g. track.csv')
 
+    parser.add_argument('--sequence_name', type=str, default='untitled')
     parser.add_argument('--init_rect', type=json.loads, required=True,
         help='e.g. {"xmin": 0.1, "ymin": 0.7, "xmax": 0.4, "ymax": 0.9}')
     parser.add_argument('--start', type=int, required=True)
     parser.add_argument('--end', type=int, required=True)
     parser.add_argument('--image_format', type=str, required=True,
         help='e.g. sequence/%%06d.jpeg')
+
+    parser.add_argument('--gpu_frac', type=float, default='1.0', help='fraction of gpu memory')
+    parser.add_argument('--vis', action='store_true')
+    parser.add_argument('--vis_dir', type=str)
+    parser.add_argument('--vis_keep_frames', action='store_true')
 
     return parser.parse_args()
 
@@ -41,13 +47,14 @@ def main():
     o.initialize()
 
     # Load sequence from args.
+    frames = range(args.start, args.end+1)
     sequence = {}
-    sequence['image_files'] = [args.image_format % i for i in range(args.start, args.end+1)]
-    sequence['viewports'] = [geom_np.unit_rect() for _ in range(args.start, args.end+1)]
+    sequence['video_name'] = args.sequence_name
+    sequence['image_files'] = [args.image_format % i for i in frames]
+    sequence['viewports'] = [geom_np.unit_rect() for _ in frames]
     init_rect = np.asfarray([args.init_rect[k] for k in ['xmin', 'ymin', 'xmax', 'ymax']])
-    sequence['labels'] = [init_rect if i == args.start else geom_np.unit_rect()
-        for i in range(args.start, args.end+1)]
-    sequence['label_is_valid'] = [True]
+    sequence['labels'] = [init_rect if i == args.start else geom_np.unit_rect() for i in frames]
+    sequence['label_is_valid'] = [i == args.start for i in frames]
     width, height = Image.open(args.image_format % args.start).size
     sequence['aspect'] = float(width) / height
     # sequence['original_image_size']
@@ -58,9 +65,13 @@ def main():
                          summaries_collections=['summaries_model'])
     saver = tf.train.Saver()
 
-    with tf.Session() as sess:
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = args.gpu_frac
+    with tf.Session(config=config) as sess:
         saver.restore(sess, args.model_file)
-        rect_pred, _ = evaluate.track(sess, example, model, sequence, use_gt=False, verbose=True)
+        rect_pred, _ = evaluate.track(sess, example, model, sequence, use_gt=False, verbose=True,
+            visualize=args.vis, vis_dir=args.vis_dir, save_frames=args.vis_keep_frames)
 
     rect_pred = np.asarray(rect_pred).tolist()
     with open(args.out_file, 'w') as f:
