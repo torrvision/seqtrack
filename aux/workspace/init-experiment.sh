2a662c2c0f8f93e7e820d5a120cc0250d314272f
Jack Valmadre <jack.valmadre@gmail.com>
2017-12-02 13:41:52 +0000

    Remove env from workspace script
    Former-commit-id: b813c114a94a8c57ca679b6378eead9a64a42e32

--- aux/workspace/init-experiment.sh
+++ aux/workspace/init-experiment.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]] ; then
-    echo "usage: $0 experiments/ name"
+if [[ "$#" -ne 2 ]] ; then
+    echo "usage: "$0" experiments/ name"
     exit 1
 fi
 
-root="$1"
+experiments_root="$1"
 name="$2"
 
 # Move to parent directory of script.
 script_file="$( readlink -f "${BASH_SOURCE[0]}" )"
 script_dir="$( dirname "$script_file" )"
-src="$( dirname "$( dirname "$script_dir" )" )"
+# root/aux/workspace/init-experiment.sh
+orig_repo_dir="$( dirname "$( dirname "$script_dir" )" )"
 
-date=$(date +%Y-%m-%d)
+date="$(date +%Y-%m-%d)"
 fullname="$date-$name"
 
-dir="$root/$fullname"
+dir="$experiments_root/$fullname"
 echo "workspace: $dir"
 
 if ! mkdir "$dir" ; then
@@ -24,7 +25,7 @@ if ! mkdir "$dir" ; then
     exit 1
 fi
 
-if ! cp $script_dir/install/* "$dir/" ; then
+if ! cp "$script_dir"/install/* "$dir/" ; then
     echo 'cannot copy workspace scripts'
     exit 1
 fi
@@ -36,31 +37,33 @@ if ! touch "$dir/workspace/run.sh" ; then
     echo 'cannot touch run.sh'
     exit 1
 fi
-if ! (cd $src && git log -1 --format="%H") >"$dir/commit.txt" ; then
+if ! (cd "$script_dir" && git log -1 --format="%H") >"$dir/commit.txt" ; then
     echo 'cannot get git commit'
     exit 1
 fi
-if ! (cd $src && git config --get remote.local.url) >"$dir/remote.txt" ; then
+if ! (cd "$script_dir" && git config --get remote.local.url) >"$dir/remote.txt" ; then
     echo 'cannot get git remote url'
     exit 1
 fi
-# Optional local virtual environment.
-if [ -d $src/env ] ; then
-    if ! (cd $src && env/bin/pip freeze) >"$dir/requirements.txt" ; then
-        echo 'cannot get pip requirements'
-        exit 1
-    fi
-    touch "$src/env/pip.conf"
-    if ! cp "$src/env/pip.conf" "$dir/pip.conf" ; then
-        echo 'cannot copy pip configuration'
-        exit 1
-    fi
-fi
 
-( cd $dir &&  ./create-experiment.sh )
+# # Optional local virtual environment.
+# if [ -d "$orig_repo_dir/env" ] ; then
+#     if ! "$orig_repo_dir/env/bin/pip" freeze >"$dir/requirements.txt" ; then
+#         echo 'cannot get pip requirements'
+#         exit 1
+#     fi
+#     touch "$orig_repo_dir/env/pip.conf"
+#     if ! cp "$orig_repo_dir/env/pip.conf" "$dir/pip.conf" ; then
+#         echo 'cannot copy pip configuration'
+#         exit 1
+#     fi
+# fi
 
-# TODO: Come up with a better place to put aux/ files?
-if ! rsync -a "$src/seqtrack/aux/" "$dir/repo/aux/" ; then
+( cd "$dir" &&  ./create-experiment.sh )
+
+# TODO: Come up with a better place to put aux/ files? Maybe with data?
+aux_data_dir=seqtrack/aux
+if ! rsync -a "$orig_repo_dir/$aux_data_dir/" "$dir/repo/$aux_data_dir/" ; then
     echo 'cannot copy auxiliary files'
     exit 1
 fi
