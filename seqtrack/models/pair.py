d6c3fd13bd04bacb33357af3893eb4041e046e20
Jack Valmadre <jack.valmadre@gmail.com>
2017-12-24 17:52:20 +0000

    Apply same updates to pair net
    Former-commit-id: da3640206c91eb99fbb384c149d943dcd58e5cdd

--- seqtrack/models/pair.py
+++ seqtrack/models/pair.py
@@ -46,6 +46,7 @@ class PairConcat(models_interface.IterModel):
             report_square=False,
             hann_method='none', # none, mul_prob, add_logit
             hann_coeff=1.0,
+            arg_max_eps_rel=0.05,
             # Loss parameters:
             sigma=0.2,
             balance_classes=True):
@@ -74,6 +75,7 @@ class PairConcat(models_interface.IterModel):
         self._report_square = report_square
         self._hann_method = hann_method
         self._hann_coeff = hann_coeff
+        self._arg_max_eps_rel = arg_max_eps_rel
         self._sigma = sigma
         self._balance_classes = balance_classes
 
@@ -101,8 +103,6 @@ class PairConcat(models_interface.IterModel):
     def next(self, frame, prev_state, name='timestep'):
         with tf.name_scope(name) as scope:
             gt_rect = tf.where(frame['y_is_valid'], frame['y'], prev_state['y'])
-            # During training, let the "previous location" be the current true location
-            # so that the object is in the center of the search area (like SiamFC).
             prev_rect = prev_state['y']
             # Coerce the aspect ratio of the rectangle to construct the search area.
             search_rect, _ = self._get_search_rect(prev_rect)
@@ -143,7 +143,8 @@ class PairConcat(models_interface.IterModel):
             response_final = self._finalize_scores(response, rfs['search'].stride)
             # upsample_response_size = response_final.shape.as_list()[-3:-1]
             # assert np.all(upsample_response_size <= self._search_size)
-            translation, scale = util.find_peak_pyr(response_final, scales)
+            translation, scale = util.find_peak_pyr(response_final, scales,
+                                                    eps_rel=self._arg_max_eps_rel)
             translation = translation / self._search_size
 
             vis = self._visualize_response(
@@ -295,7 +296,7 @@ class PairConcat(models_interface.IterModel):
                 response = tf.sigmoid(response)
                 response *= tf.expand_dims(hann_2d(upsample_size), -1)
             elif self._hann_method == 'none' or not self._hann_method:
-                pass
+                response = tf.sigmoid(response)
             else:
                 raise ValueError('unknown hann method: {}'.format(self._hann_method))
             return response
