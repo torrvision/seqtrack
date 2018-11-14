9e627bb8d71b2045d00bcb0ff25cbbb513c164e3
Jack Valmadre <jack.valmadre@gmail.com>
2018-11-14 16:36:20 +0000

    Add errorbars to plot
    Former-commit-id: f6860221d41d4c6a6ec504f113095a8fb085ff6d

--- seqtrack/experiments/spatial_weight.py
+++ seqtrack/experiments/spatial_weight.py
@@ -9,6 +9,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
+
 import argparse
 import collections
 import json
@@ -28,20 +30,20 @@ def main():
     logging.basicConfig(level=getattr(logging, args.loglevel.upper()))
 
     FeatureConfig = collections.namedtuple('FeatureConfig', ['arch', 'arch_params'])
-    feature_configs = {
-        'alexnet_conv2': FeatureConfig(
+    feature_configs = [
+        ('alexnet_conv2', FeatureConfig(
             arch='alexnet',
             arch_params=dict(
-                output_layer='conv2')),
-        'alexnet_conv3': FeatureConfig(
+                output_layer='conv2'))),
+        ('alexnet_conv3', FeatureConfig(
             arch='alexnet',
             arch_params=dict(
-                output_layer='conv3')),
-        'alexnet_conv5': FeatureConfig(
+                output_layer='conv3'))),
+        ('alexnet_conv5', FeatureConfig(
             arch='alexnet',
             arch_params=dict(
-                output_layer='conv5')),
-    }
+                output_layer='conv5'))),
+    ]
     use_spatial_weights = [False, True]
     desired_context_amounts = [1.5, 2.0, 3.0]
 
@@ -53,7 +55,7 @@ def main():
                     weight=weight,
                     context=context,
                     seed=seed)
-        for feat, feat_config in feature_configs.items()
+        for feat, feat_config in feature_configs
         for weight in use_spatial_weights
         for context in desired_context_amounts
         for seed in range(args.num_trials)
@@ -64,7 +66,7 @@ def main():
 
     # To obtain one number per training process, we use one dataset as validation.
     summaries = {}
-    for feat, feat_config in feature_configs.items():
+    for feat, feat_config in feature_configs:
         for weight in use_spatial_weights:
             for context in desired_context_amounts:
                 summary_name = make_name(feat=feat, weight=weight, context=context)
@@ -89,7 +91,8 @@ def main():
     fig, ax = plt.subplots()
     plt.xlabel('Template context')
     plt.ylabel('Mean IOU')
-    for feat, feat_config in feature_configs.items():
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    for feat_ind, (feat, feat_config) in enumerate(feature_configs):
         for weight in use_spatial_weights:
             name_fn = lambda context: make_name(feat=feat, weight=weight, context=context)
             contexts = [summaries[name_fn(context)]['model/template_scale']
@@ -97,7 +100,19 @@ def main():
             quality_metric = args.optimize_dataset + '_' + args.optimize_metric
             quality = [summaries[name_fn(context)][quality_metric]
                        for context in desired_context_amounts]
-            plt.scatter(contexts, quality, label=feat + (' (weight)' if weight else ''))
+            try:
+                variance = [summaries[name_fn(context)][quality_metric + '_var']
+                            for context in desired_context_amounts]
+                error = 1.64485 * np.sqrt(variance)
+            except KeyError:
+                error = None
+            if not weight:
+                plt.fill_between(x=contexts, y1=quality - error, y2=quality + error,
+                                 color=colors[feat_ind], label=None, alpha=0.2)
+            plt.errorbar(x=contexts, y=quality, yerr=None,
+                         color=colors[feat_ind],
+                         label=feat if not weight else None,
+                         linestyle='dashed' if weight else 'solid')
     ax.legend()
     plt.savefig('plot.pdf')
 
