7ecafda4b61a09dbe381aec75483a9db271b871e
Jack Valmadre <jack.valmadre@gmail.com>
2018-12-19 00:38:57 +0000

    Add OTB comparison test
    Former-commit-id: 734fb3e3a8c0b05923642504045d85427042b3c9

--- seqtrack/test_assess.py
+++ seqtrack/test_assess.py
@@ -5,9 +5,15 @@ from __future__ import print_function
 import unittest
 
 import numpy as np
+import os
 
 from seqtrack import geom_np
 from seqtrack import assess
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(FILE_DIR, '..'))
+OTB_DATA_DIR = os.path.join(PROJECT_DIR, 'aux', 'otb', 'test_data')
+OTB_TRACKERS = [('KCF', 0.477), ('Staple', 0.581), ('CCOT', 0.671)]
 
 
 class TestAssess(unittest.TestCase):
@@ -34,13 +40,75 @@ class TestAssess(unittest.TestCase):
     def test_assess_dataset(self):
         num_seqs = 50
         seqs, preds = random_dataset_and_predictions(np.random.RandomState(0), num_seqs)
-        subseqs = {'OPE': {name: [name] for name in seqs.keys()}}
-        metrics = assess.assess_dataset(seqs, preds, subseqs)
+        metrics = assess.assess_dataset(seqs, preds)
         self.assertLessEqual(0, metrics['OPE_iou_seq_mean'])
         self.assertLessEqual(metrics['OPE_iou_seq_mean'], 1)
         for key in metrics:
             self.assertTrue(np.isscalar(metrics[key]),
                             'not scalar \'{}\': {}'.format(key, metrics[key]))
+
+    def test_assess_frames_vs_otb(self):
+        '''Compares per-frame IOU against OTB benchmark.'''
+        seqs = _load_otb_sequences()
+        for tracker, _ in OTB_TRACKERS:
+            preds, ious = _load_otb_predictions(tracker, list(seqs.keys()))
+            for seq_name in seqs.keys():
+                frame_metrics = assess.assess_frames(seqs[seq_name], preds[seq_name])
+                np.testing.assert_allclose(frame_metrics['iou'], ious[seq_name], atol=1e-4)
+
+    def test_assess_dataset_vs_otb(self):
+        '''Compares area-under-curve against OTB benchmark.'''
+        seqs = _load_otb_sequences()
+        for tracker, desired_auc in OTB_TRACKERS:
+            preds, ious = _load_otb_predictions(tracker, list(seqs.keys()))
+            metrics = assess.assess_dataset(seqs, preds)
+            np.testing.assert_almost_equal(metrics['OPE_iou_success_auc_seq_mean'],
+                                           desired_auc, decimal=3)
+
+
+def _load_otb_sequences():
+    names = _read_lines(os.path.join(OTB_DATA_DIR, 'sequences.txt'))
+    seqs = {}
+    for name in names:
+        fname = os.path.join(OTB_DATA_DIR, 'groundtruth', name + '.txt')
+        rects = np.loadtxt(fname, delimiter=',')
+        seq_len = len(rects)
+        is_valid = np.all(rects > 0, axis=-1)
+        rects = _convert_from_otb(rects)
+        seqs[name] = {
+            'image_files': [None for t in range(seq_len)],
+            'labels': rects,
+            'label_is_valid': is_valid,
+            'aspect': [None for t in range(seq_len)],
+            'video_name': name,
+        }
+    return seqs
+
+
+def _convert_from_otb(rect, axis=-1):
+    min_pt, size = np.split(rect, [2], axis=axis)
+    return geom_np.make_rect(min_pt, min_pt + size)
+
+
+def _load_otb_predictions(tracker, seq_names):
+    preds = {}
+    ious = {}
+    for name in seq_names:
+        fname = os.path.join(OTB_DATA_DIR, 'predictions', tracker, name + '.txt')
+        data = np.loadtxt(fname, delimiter=',')
+        seq_len = len(data)
+        preds[name] = _convert_from_otb(data[1:, 0:4])
+        iou = data[1:, 4]
+        # OTB uses IOU = -1 for missing annotation.
+        iou[iou < 0] = np.nan
+        ious[name] = iou
+    return preds, ious
+
+
+def _read_lines(fname):
+    with open(fname, 'r') as f:
+        lines = [s.strip() for s in f.readlines()]
+    return [s for s in lines if s]
 
 
 def random_sequence_and_predictions(rand, num_frames, min_size=0.1, max_size=0.5,
@@ -69,5 +137,5 @@ def random_dataset_and_predictions(rand, num_seqs, min_len=200, max_len=400, **k
     lens = [int(round(x)) for x in rand.uniform(min_len, max_len, size=num_seqs)]
     seqs, preds = zip(*[random_sequence_and_predictions(rand, lens[i], **kwargs)
                         for i in range(num_seqs)])
-    names = list(map(str, range(num_seqs)))
+    names = ['seq_{:d}'.format(i) for i in range(num_seqs)]
     return dict(zip(names, seqs)), dict(zip(names, preds))
